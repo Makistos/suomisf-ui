5ebb00e880740506a0b62e5689f2124b58291214
Marko Poutiainen <regs@sofistes.net>
2022-01-25 23:58:47 +0200

    Lots of improvements to people listing page. Small tweaks still required.

--- src/People.tsx
+++ src/People.tsx
@@ -10,53 +10,79 @@ import { Column, ColumnDataType, ColumnFilterElementType } from 'primereact/colu
 import { Dropdown } from 'primereact/dropdown';
 import { DataTableSortOrderType, DataTablePFSEvent } from 'primereact/datatable';
 import { InputText } from "primereact/inputtext";
+import { InputNumber } from 'primereact/inputnumber';
 
 const baseURL = 'people';
 
 export const People = () => {
     const user = getCurrenUser();
     const [people, setPeople]: [IPerson[], (people: IPerson[]) => void] = React.useState<IPerson[]>([]);
-    const [countries, setCountries]: [ICountry[], (countries: ICountry[]) => void] = React.useState<ICountry[]>([]);
+    const [countries, setCountries]: [string[], (countries: string[]) => void] = React.useState<string[]>([]);
+    const [roles, setRoles]: [string[], (roles: string[]) => void] = React.useState<string[]>([]);
     const [filters, setFilters] = React.useState({
         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
-        dob: { value: null, matchMode: FilterMatchMode.EQUALS },
-        dod: { value: null, matchMode: FilterMatchMode.EQUALS },
+        name: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+        dob: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+        dod: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
         nationality: { value: null, matchMode: FilterMatchMode.EQUALS },
-        roles: { value: null, matchMode: FilterMatchMode.CONTAINS }
+        work_count: { value: null, matchMode: FilterMatchMode.EQUALS },
+        story_count: { value: null, matchMode: FilterMatchMode.EQUALS },
+        roles: { value: null, matchMode: FilterMatchMode.EQUALS }
     });
+    const [loading, setLoading] = React.useState(true);
     const [globalFilterValue, setGlobalFilterValue] = React.useState("");
 
     const [sortField, setSortField] = React.useState("name")
     const [sortOrder, setSortOrder] = React.useState(0);
 
     React.useEffect(() => {
+        const getRoles = (people: IPerson[]) => {
+            let _roles: string[] = [];
+            for (let i = 0; i < people.length; i++) {
+                for (let j = 0; j < people[i].roles.length; j++) {
+                    _roles.push(people[i].roles[j]);
+                }
+            }
+            let _unique = new Set(_roles);
+            setRoles(Array.from(_unique));
+        }
         async function getPeople() {
             let url = baseURL;
             try {
                 const response = await getApiContent(url, user);
                 setPeople(response.data);
-
+                getRoles(response.data);
+                getCountries(response.data);
+                setLoading(false);
             }
             catch (e) {
                 console.error(e);
             }
         }
-        async function getCountries() {
-            let url = "countries";
+        const getCountries = (people: IPerson[]) => {
+            /*let url = "countries";
             try {
                 const response = await getApiContent(url, user);
                 let _countries: ICountry[] = []
                 for (let i = 0; i < response.data.length; i++) {
                     _countries.push(response.data[i].name);
                 }
+
                 setCountries(_countries);
             }
             catch (e) {
                 console.error(e);
+            }*/
+            let _countries: string[] = [];
+            for (let i = 0; i < people.length; i++) {
+                let country = people[i].nationality;
+                if (country !== null && country !== undefined) {
+                    _countries.push(country);
+                }
             }
+            setCountries(Array.from(new Set(_countries)).sort());
         }
-        getCountries();
+        //getCountries();
         getPeople();
     }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
@@ -92,6 +118,30 @@ export const People = () => {
         );
     }
 
+    const shortStoryFilterTemplate: ColumnFilterElementType = (options) => {
+        if (options === null || options === undefined) {
+            return <div></div>
+        }
+        return (
+            <InputNumber value={options.value} mode='decimal'
+                onChange={(e) => options.filterApplyCallback(e.value, options.index)}
+                className="p-column-filter"
+                placeholder="Lukumäärä"
+            />
+        );
+
+    }
+    const roleFilterTemplate: ColumnFilterElementType = (options) => {
+        if (options === null || options === undefined) {
+            return <div></div>
+        }
+        return (
+            <Dropdown value={options.value} options={roles}
+                onChange={(e) => options.filterApplyCallback(e.value)}
+                className="p-column-filter" showClear
+            />
+        );
+    }
     const nationalityBodyTemplate = (rowData: IPerson) => {
 
     }
@@ -100,16 +150,20 @@ export const People = () => {
 
     return (
         <div>
+            <h1 className='title'>Henkilöluettelo</h1>
             {people !== null && people !== undefined ? (
                 <DataTable value={people}
                     sortField="name"
                     sortOrder={1}
                     size="small"
+                    globalFilterFields={["name", "dob", "dod", "nationality", "work_count", "story_count", "roles"]}
                     responsiveLayout="scroll"
                     dataKey="id"
                     filters={filters}
-                    filterDisplay="row"
+                    filterDisplay="menu"
                     rows={50}
+                    emptyMessage="Henkilöitä ei löytynyt"
+                    loading={loading}
                 >
                     <Column field="name" header="Nimi" body={nameTemplate}
                         filter
@@ -130,11 +184,29 @@ export const People = () => {
                         filter
                         filterPlaceholder="Kansallisuus"
                         filterElement={nationalityFilterTemplate}
+                        showFilterMatchModes={false}
                         sortable>
+                    </Column>
+                    <Column field="work_count" header="Teoksia"
+                        filter
+                        filterPlaceholder="Teoksia"
+                        dataType="numeric"
+                        filterElement={shortStoryFilterTemplate}
+                        sortable>
+                    </Column>
+                    <Column field="story_count" header="Novelleja"
+                        filter
+                        filterPlaceholder="Novelleja"
+                        dataType="numeric"
+                        sortable
+                        filterElement={shortStoryFilterTemplate}
+                    >
                     </Column>
                     <Column field="roles" header="Roolit" body={roleTemplate}
                         filter
                         filterPlaceholder="Rooli"
+                        filterElement={roleFilterTemplate}
+                        showFilterMatchModes={false}
                         sortable>
                     </Column>
                 </DataTable>

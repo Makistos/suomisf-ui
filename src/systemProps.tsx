f336b44b36bb3310f72196851689469f197fd01f
Marko Poutiainen <regs@sofistes.net>
2022-04-15 19:48:09 +0300

    Added IMAGE_URL to further help with paths.

--- src/systemProps.tsx
+++ src/systemProps.tsx
@@ -2,3 +2,4 @@
 //export const API_URL = 'http://localhost:5000/';
 export const SITE_URL = 'http://www.sf-bibliografia.fi:3000/';
 export const API_URL = 'http://www.sf-bibliografia.fi/api/';
+export const IMAGE_URL = 'http://www.sf-bibliografia.fi/';

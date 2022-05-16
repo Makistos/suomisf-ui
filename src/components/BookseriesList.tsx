616ce61b3b94fab739831d67704c84d6ad0cb9f5
Marko Poutiainen <regs@sofistes.net>
2022-05-16 20:58:09 +0300

    Added linking to serieslist series headers.

--- src/components/BookseriesList.tsx
+++ src/components/BookseriesList.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import { IWork, WorkSummary } from "./Work";
 import { groupByBookSeries } from "./Bookseries";
@@ -9,9 +10,23 @@ interface SeriesListProps {
 export const SeriesList = ({ works }: SeriesListProps) => {
     const [groupedSeries, setGroupedSeries]: [Record<string, IWork[]>,
         (works: Record<string, IWork[]>) => void] = useState({});
+    const [bookseries, setBookseries]: [Record<string, number>,
+        (bookseries: Record<string, number>) => void] = useState({});
+
+    const findBookseries = (works: IWork[]) => {
+        /* Create a dictionary which maps bookseries names with bookseries ids.
+           This is needed for creating links to bookseries.
+         */
+        let retval = {};
+        retval = Object.assign({},
+            ...works.filter(work => work.bookseries)
+                .map(work => ({ [work.bookseries.name]: work.bookseries.id })));
+        return retval;
+    }
 
     useEffect(() => {
         setGroupedSeries(groupByBookSeries(works.filter(work => work.bookseries !== null)));
+        setBookseries(findBookseries(works));
     }, [works])
 
     return (
@@ -20,8 +35,11 @@ export const SeriesList = ({ works }: SeriesListProps) => {
                 Object.entries(groupedSeries)
                     .map(([seriesName, ser]) => {
                         return (
-                            <div>
-                                <h3>{seriesName}</h3>
+                            <div key={bookseries[seriesName]}>
+                                <h3><Link to={`/bookseries/${bookseries[seriesName]}`} key={bookseries[seriesName]}>
+                                    {seriesName}
+                                </Link>
+                                </h3>
                                 {
                                     ser.sort((a, b) => a.pubyear < b.pubyear ? -1 : 1).map((work: IWork) => (
                                         <WorkSummary work={work} key={work.id}

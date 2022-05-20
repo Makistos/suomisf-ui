bb5b745e384b42141540775ebaa10bb6b9abad4b
Marko Poutiainen <regs@sofistes.net>
2022-05-20 21:56:51 +0300

    Removed tabs with no contents from BookControl.

--- src/components/BookControl.tsx
+++ src/components/BookControl.tsx
@@ -10,24 +10,33 @@ interface CBCProps {
 }
 
 export const ContributorBookControl = ({ person }: CBCProps) => {
+
     const headerText = (staticText: string, count: number) => {
         return staticText + " (" + count + ")";
     }
     return (
         <Fieldset legend="Kirjat" toggleable>
             <TabView>
-                <TabPanel header={headerText("Kirjoittanut", person.works.length)}>
-                    <WorkList works={person.works} personName={person.name} />
-                </TabPanel>
-                <TabPanel header={headerText("Toimittanut", person.edits.length)}>
-                    <EditionList editions={person.edits} person={person} />
-                </TabPanel>
-                <TabPanel header={headerText("Kääntänyt", person.translations.length)}>
-                    <EditionList editions={person.translations} person={person} />
-                </TabPanel>
-                <TabPanel header="Sarjat">
-                    <SeriesList works={person.works} seriesType='bookseries' />
-                </TabPanel>
+                {person.works.length > 0 &&
+                    <TabPanel header={headerText("Kirjoittanut", person.works.length)}>
+                        <WorkList works={person.works} personName={person.name} />
+                    </TabPanel>
+                }
+                {person.edits.length > 0 &&
+                    <TabPanel header={headerText("Toimittanut", person.edits.length)}>
+                        <EditionList editions={person.edits} person={person} />
+                    </TabPanel>
+                }
+                {person.translations.length > 0 &&
+                    <TabPanel header={headerText("Kääntänyt", person.translations.length)}>
+                        <EditionList editions={person.translations} person={person} />
+                    </TabPanel>
+                }
+                {person.works.some((work) => work.bookseries !== null) &&
+                    <TabPanel header="Sarjat">
+                        <SeriesList works={person.works} seriesType='bookseries' />
+                    </TabPanel>
+                }
             </TabView>
         </Fieldset>
     )

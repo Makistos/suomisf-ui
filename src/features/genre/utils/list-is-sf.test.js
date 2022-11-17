025f6aa9f502f78bbd96868fc09a24e4e313dad6
Marko Poutiainen <Marko.Poutiainen@bittium.com>
2022-11-17 15:50:07 +0200

    Created test-data directory for holding common test data.

--- src/features/genre/utils/list-is-sf.test.js
+++ src/features/genre/utils/list-is-sf.test.js
@@ -1,23 +1,14 @@
 import { listIsSf } from "./list-is-sf";
-
-const allSfData = [
-  { id: 0, abbr: "SF" },
-  { id: 3, abbr: "kok" },
-];
-const nonSf = [{ id: 1, abbr: "eiSF" }];
-const sfWithNonSf = [
-  { id: 0, abbr: "SF" },
-  { id: 1, abbr: "eiSF" },
-];
+import * as data from "../../../test-data";
 
 test("Check when every item is SF and has collection", () => {
-  expect(listIsSf(allSfData)).toBe(true);
+  expect(listIsSf(data.allSfData)).toBe(true);
 });
 
 test("Check when work is not SF", () => {
-  expect(listIsSf(nonSf)).toBe(false);
+  expect(listIsSf(data.nonSf)).toBe(false);
 });
 
 test("Check when both SF and nonSF are present", () => {
-  expect(listIsSf(sfWithNonSf)).toBe(true);
+  expect(listIsSf(data.sfWithNonSf)).toBe(true);
 });
---
+++ src/test-data/genres.ts
@@ -0,0 +1,12 @@
+export const allSfData = [
+    { id: 0, abbr: "SF" },
+    { id: 3, abbr: "kok" },
+];
+
+export const nonSf = [{ id: 1, abbr: "eiSF" }];
+
+export const sfWithNonSf = [
+    { id: 0, abbr: "SF" },
+    { id: 1, abbr: "eiSF" },
+];
+

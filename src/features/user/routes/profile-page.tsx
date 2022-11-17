ba7599a050d474017933f0954a7cfd44e1abf3cb
Marko Poutiainen <Marko.Poutiainen@bittium.com>
2022-11-17 10:51:04 +0200

    Refactor: Moved user profile page to user feature dir.

---
+++ src/features/user/routes/profile-page.tsx
@@ -0,0 +1,13 @@
+import { getCurrenUser } from "../../../services/auth-service";
+
+const ProfilePage = () => {
+    const currentUser = getCurrenUser();
+
+    return (
+        <div className="container">
+
+        </div>
+    );
+}
+
+export default ProfilePage;

4845144cf29684c9a6cdaf8732cfe373410ec3a1
Marko Poutiainen <Marko.Poutiainen@bittium.com>
2022-11-30 12:59:19 +0200

    Second try at fixing tests, still no dice.

--- src/features/publisher/routes/publisher-page.test.js
+++ src/features/publisher/routes/publisher-page.test.js
@@ -1,31 +1,40 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 
 import { screen, waitFor } from "@testing-library/react";
 
 import { PublisherPage } from "./publisher-page";
 import { renderWithRouter } from "../../../testing";
 import { flushSync } from "react-dom";
+import { renderWithClient } from "../../../testing/render-with-client";
 
 const queryClient = new QueryClient();
-
+export function sleep(timeout) {
+  return new Promise((resolve, _reject) => {
+    setTimeout(resolve, timeout);
+  });
+}
 jest.setTimeout(20000);
-
 describe("PublisherPage", () => {
   it("renders PublisherPage", async () => {
-    <QueryClientProvider client={queryClient}>
-      renderWithRouter(
-      <PublisherPage id={2} />
-      );
-    </QueryClientProvider>;
-    await waitFor(
-      () => {
-        //expect(screen.getByTestId("short-name")).toMatch("Atena");
-        expect(screen.getByText(/Absurdia/i).textContent).toMatch(/Absurdia/),
-          { timeout: 20000 };
-        screen.debug();
-      },
-      { timeout: 20000 }
-    );
+    renderWithClient(queryClient, <PublisherPage id={2} />);
+    screen.debug();
+    //   <QueryClientProvider client={queryClient}>
+    //     renderWithRouter(
+    //     <PublisherPage id={2} />
+    //     );
+    //   </QueryClientProvider>;
+    sleep(20000);
+    screen.debug();
+    // await waitFor(
+    //   () => {
+    //     //expect(screen.getByTestId("short-name")).toMatch("Atena");
+    //     expect(screen.getByText(/Absurdia/i).textContent).toMatch(/Absurdia/),
+    //       { timeout: 20000 };
+    //     screen.debug();
+    //   },
+    //   { timeout: 20000 }
+    // );
+    expect(screen.getByText(/Absurdia/i).textContent).toMatch(/Absurdia/);
   });
 });

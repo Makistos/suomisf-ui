40b926993457fb82f59e2d7b82dfb10778af9d7c
Marko Poutiainen <regs@sofistes.net>
2023-08-15 21:34:45 +0300

    Fixed API post and put functions to work better with useMutate.

--- src/services/user-service.tsx
+++ src/services/user-service.tsx
@@ -52,22 +52,21 @@ export const getApiContent = (url: string, user: User | null) => {
     }
 }
 
-const postPublicContent = async (url: string, dataOut: any, callback: cbType | null, user: User | null): Promise<HttpStatusResponse> => {
+const postContent = async (url: string, dataOut: any, callback: cbType | null, user: User | null): Promise<HttpStatusResponse> => {
     const addr = process.env.REACT_APP_API_URL + url;
     let error_msg = "";
     let status = 0;
-    //let retval: HttpStatusResponse = { response: '', status: 0 };
     try {
-        const { data } = await axios.post<HttpStatusResponse>(addr, dataOut, { headers: authHeader() })
-        //console.log(data)
-        return data
-        // .then((response) => {
-        //     if (callback) {
-        //         callback(response.data);
-        //     }
-        //     console.log("Post response: " + response.data)
-        //     return response.data;
-        // })
+        const response = await axios.post(addr, dataOut, { headers: authHeader() });
+        const retval: HttpStatusResponse = {
+            response: response.data,
+            status: response.status
+        }
+        if (response.status === 201 || response.status === 200) {
+            return retval;
+        } else {
+            throw new Error(response.data);
+        }
     } catch (err: any) {
         if (err.response) {
             error_msg = JSON.stringify(err.response, null, 2);
@@ -81,33 +80,52 @@ const postPublicContent = async (url: string, dataOut: any, callback: cbType | n
             error_msg = JSON.stringify(err, null, 2);
             console.log("other error: " + error_msg);
         }
-        return { response: error_msg, status: 0 }
     }
+    return new Promise<HttpStatusResponse>((resolve, reject) => {
+        const retval: HttpStatusResponse = {
+            response: error_msg,
+            status: status
+        }
+        reject(retval);
+    })
 }
 
 export const putContent = async (url: string, data: any, user: User | null): Promise<HttpStatusResponse> => {
     const addr = process.env.REACT_APP_API_URL + url;
     let error_msg = "";
     let status = 0;
-    await axios.put(addr, data, { headers: authHeader() })
-        .then((response) => {
-            return response.data;
-        })
-        .catch((err: AxiosError) => {
-            if (err.response) {
-                error_msg = JSON.stringify(err.response, null, 2);
-                status = err.response.status;
-                console.log("server error: " + error_msg);
-            } else if (err.request) {
-                error_msg = JSON.stringify(err.request, null, 2);
-                status = err.request.status;
-                console.log("no response: " + error_msg);
-            } else {
-                error_msg = JSON.stringify(err, null, 2);
-                console.log("other error: " + error_msg);
-            }
-        })
-    return { response: error_msg, status: 0 };
+    try {
+        const response = await axios.put(addr, data, { headers: authHeader() });
+        const retval: HttpStatusResponse = {
+            response: response.data,
+            status: response.status
+        }
+        if (response.status === 200 || response.status === 201) {
+            return retval;
+        } else {
+            throw new Error(response.data);
+        }
+    } catch (err: any) {
+        if (err.response) {
+            error_msg = JSON.stringify(err.response, null, 2);
+            status = err.response.status;
+            console.log("server error: " + error_msg);
+        } else if (err.request) {
+            error_msg = JSON.stringify(err.request, null, 2);
+            status = err.request.status;
+            console.log("no response: " + error_msg);
+        } else {
+            error_msg = JSON.stringify(err, null, 2);
+            console.log("other error: " + error_msg);
+        }
+    }
+    return new Promise<HttpStatusResponse>((resolve, reject) => {
+        const retval: HttpStatusResponse = {
+            response: error_msg,
+            status: status
+        }
+        reject(retval);
+    })
 }
 
 export const deleteApiContent = async (url: string): Promise<HttpStatusResponse> => {
@@ -149,7 +167,7 @@ export const deleteApiContent = async (url: string): Promise<HttpStatusResponse>
 }
 
 export const postApiContent = (url: string, data: any, user: User | null) => {
-    return postPublicContent(url, data, null, user)
+    return postContent(url, data, null, user)
 }
 
 export const putApiContent = (url: string, data: any, user: User | null) => {

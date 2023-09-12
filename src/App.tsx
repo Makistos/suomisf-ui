e8299fd289101cd85aa118b228d6cee27a6ffa4b
Marko Poutiainen <regs@sofistes.net>
2023-09-12 21:54:20 +0300

    Fixed application title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -67,11 +67,11 @@ function App() {
     }
   });
 
-  // const [title, setTitle] = useState("SF-Bibliografia");
+  const [title, setTitle] = useState("SF-Bibliografia");
 
-  // useEffect(() => {
-  //   document.title = title;
-  // }, [title]);
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
 
   return (
     <QueryClientProvider client={queryClient}>

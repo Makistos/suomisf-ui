c9bd938b4f2d0d12203956ac12a58ff94815b42d
Marko Poutiainen <regs@sofistes.net>
2024-07-02 22:28:42 +0300

    Some fixes to home page layout. Should work better with smaller screens.

--- src/home.tsx
+++ src/home.tsx
@@ -41,7 +41,7 @@ export const Home = () => {
 
   return (
     <div className="grid mt-4 p-4">
-      <div className="col-12 lg:col-8">
+      <div className="grid col-12 lg:col-8">
         <div className="front-text-block">
           <h1 className="frontpage mb-8">Tervetuloa suomenkielisen spekulatiivisen fiktion bibliografiaan!</h1>
           <p className="frontpage mt-4">
@@ -60,38 +60,34 @@ export const Home = () => {
             <div className="col-12">
               Viimeisimmät lisäykset
             </div>
-            <div className="col-12">
-              <div className="flex">
-                {stats && stats.latest && stats.latest.map((edition) => (
-                  <div className="col-3 justify-content-between" key={edition.id}>
-                    <div className="col-12" >
-                      {/* {edition.images.length > 0 && ( */}
-                      <div>
-                        <Tooltip target={".image-" + edition.id} position="top" mouseTrack mouseTrackLeft={10}>
-                          <div className="flex align-items-center">
-                            <ImageTooltip edition={edition} />
-                          </div>
-                        </Tooltip>
+            <div className="flex flex-wrap justify-content-between align-items-center flex-grow-1 gap-3">
+              {stats && stats.latest && stats.latest.map((edition) => (
+                <div className="flex flex-grow justify-content-between gap-1" key={edition.id}>
+                  <div>
+                    <div>
+                      <Tooltip target={".image-" + edition.id} position="top" mouseTrack mouseTrackLeft={10}>
+                        <div className="flex align-items-center">
+                          <ImageTooltip edition={edition} />
+                        </div>
+                      </Tooltip>
 
-                        <Link to={`/works/${edition.work[0].id}`}>
-                          {edition.images.length > 0 && edition.images[0].image_src !== undefined && edition.images[0].image_src !== null &&
-                            edition.images[0].image_src !== "" ?
-                            <><img alt={edition.title} className={"image-" + edition.id} src={import.meta.env.VITE_IMAGE_URL + edition.images[0].image_src}
-                              height={String(edition.size ? Number(edition.size) * 10 : 200)} />
-                            </>
-                            :
-                            <div>
-                              {edition.work[0].author_str}:<br />
-                              {edition.title}
-                            </div>
-                          }
-                        </Link>
-                      </div>
-                      {/* )} */}
+                      <Link to={`/works/${edition.work[0].id}`}>
+                        {edition.images.length > 0 && edition.images[0].image_src !== undefined && edition.images[0].image_src !== null &&
+                          edition.images[0].image_src !== "" ?
+                          <><img alt={edition.title} className={"image-" + edition.id} src={import.meta.env.VITE_IMAGE_URL + edition.images[0].image_src}
+                            height={String(edition.size ? Number(edition.size) * 10 : 200)} />
+                          </>
+                          :
+                          <div>
+                            {edition.work[0].author_str}:<br />
+                            {edition.title}
+                          </div>
+                        }
+                      </Link>
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
           </div>
           <div className="grid justify-content-end p-1">
@@ -99,41 +95,39 @@ export const Home = () => {
           </div>
         </div>
         {stats && (
-          <div className="stats">
-            <div className="flex justify-content-between p-3">
-              <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
-                <div className="col-12 justify-content-center p-0">Teoksia</div>
-                <div className="col-12 justify-content-center stats-number pt-2">{stats.works}</div>
-              </div>
-              <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
-                <div className="col-12 justify-content-center p-0">Painoksia</div>
-                <div className="col-12 justify-content-center stats-number pt-2">{stats.editions}</div>
-              </div>
-              <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
-                <div className="col-12 justify-content-center p-0">Novelleja</div>
-                <div className="col-12 justify-content-center stats-number pt-2">{stats.shorts}</div>
-              </div>
-              <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
-                <div className="col-12 justify-content-center p-0">Kansia</div>
-                <div className="col-12 justify-content-center stats-number pt-2">{stats.covers}</div>
-              </div>
-              <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
-                <div className="col-12 justify-content-center p-0">Lehtiä</div>
-                <div className="col-12 justify-content-center stats-number pt-2">{stats.magazines}</div>
-              </div>
+          <div className="flex justify-content-between p-3 flex-grow-1 flex-shrink-1 flex-wrap stats">
+            <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
+              <div className="col-12 justify-content-center p-0">Teoksia</div>
+              <div className="col-12 justify-content-center stats-number pt-2">{stats.works}</div>
+            </div>
+            <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
+              <div className="col-12 justify-content-center p-0">Painoksia</div>
+              <div className="col-12 justify-content-center stats-number pt-2">{stats.editions}</div>
+            </div>
+            <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
+              <div className="col-12 justify-content-center p-0">Novelleja</div>
+              <div className="col-12 justify-content-center stats-number pt-2">{stats.shorts}</div>
+            </div>
+            <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
+              <div className="col-12 justify-content-center p-0">Kansia</div>
+              <div className="col-12 justify-content-center stats-number pt-2">{stats.covers}</div>
+            </div>
+            <div className="front-stats-box pt-2 pl-3 pr-3 pb-0 justify-content-center">
+              <div className="col-12 justify-content-center p-0">Lehtiä</div>
+              <div className="col-12 justify-content-center stats-number pt-2">{stats.magazines}</div>
             </div>
           </div>
         )}
-        <div className="mt-8">
-          <hr></hr>
-          <div className="grid justify-content-center vertical-align-bottom">
-            <span className="mr-3"><a href="#">Tietoja</a></span>
-            <span className="mr-3"><a href="#">Tilastot</a></span>
+        <div className="grid col-12 mt-8">
+          <hr />
+          <div className="flex gap-3 flex-grow-1 flex-shrink-1 flex-wrap justify-content-center">
+            <span><a href="#">Tietoja</a></span>
+            <span><a href="#">Tilastot</a></span>
             <span><a href="/changes">Muutokset</a></span>
           </div>
         </div>
       </div>
-      <div className="lg:col-4 mt-4">
+      <div className="grid lg:col-4 mt-4 hidden lg:block">
         <img alt="Image from a library" className="front-page-image" src="pexels-gunnar-ridderström-4318441.jpg"></img>
       </div>
     </div>

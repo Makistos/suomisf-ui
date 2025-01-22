81539ca33f1c51935ac14f84623e420e4284cf70
Marko Poutiainen <regs@sofistes.net>
2025-01-22 21:25:07 +0200

    Make getMagazine function always return a value.

--- src/api/magazine/get-magazine.ts
+++ src/api/magazine/get-magazine.ts
@@ -2,8 +2,7 @@ import { Magazine } from "@features/magazine";
 import { User } from "@features/user";
 import { getApiContent } from "@services/user-service";
 
-export const getMagazine = async (id: string | null, user: User | null): Promise<Magazine | null> => {
-    if (!id) { return null; }
-    const retval = await getApiContent('magazines/' + id, user).then(response => response.data);
+export const getMagazine = async (id: string | null, user: User | null): Promise<Magazine> => {
+    const retval = getApiContent(`magazines/${id}`, user).then(response => response.data);
     return retval;
 }
